636873861dc8571dbbafdcc719963fe32845fd9d
APURV SINHA <119706011+xeylous@users.noreply.github.com>
2024-09-27 11:29:03 +0530

    Update whatIsMyUserAgent.js

--- What Is My UserAgent/src/components/Xmltojson/whatIsMyUserAgent.js
+++ What Is My UserAgent/src/components/Xmltojson/whatIsMyUserAgent.js
@@ -10,13 +10,16 @@ const UserAgentDisplay = () => {
   }, []);
 
   return (
-    <div className="min-h-screen bg-gray-800 text-gray-300 flex items-center justify-center">
+    <>
+    <h1 className="text-4xl font-semibold mb-4 flex justify-center text-sky-300">What is your User Agent</h1>
+    <div className=" bg-transparent text-gray-300 flex items-center justify-center">
       <div className="p-6 bg-gray-900 rounded-lg shadow-lg">
         <h1 className="text-2xl font-semibold mb-4">Your User Agent</h1>
         <p className="text-lg mb-2">The user agent string of your browser is:</p>
         <pre className="bg-gray-700 p-4 rounded mt-2 overflow-x-auto">{userAgent}</pre>
       </div>
     </div>
+    </>
   );
 };
 
